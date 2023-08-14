0165f22fa538ee8c112fefaf708a9df03f48431c
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-13 20:23:16 -1100

    Add comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,103 @@
-import Header from "./Header"
-import Interaction from "./Interactions"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addReply } from "../redux/comment/commentsSlice";
 
-export default function Comment() {
+export default function Comment({
+  content, 
+  handleUpvote, 
+  handleDownvote, 
+  username,
+  createdAt,
+  score,
+  user_image,
+  commentId,
+}) {
+  const [showReplyInput, setShowReplyInput] = useState(false)
+  const [replyInput, setReplyInput] = useState('@'+username+',')
+  const currentUser = useSelector((state) => state.comment.currentUser);
+  const dispatch = useDispatch()
+
+  function handleReplySubmit(e) {
+    e.preventDefault()
+
+    // avoid multiple mentions
+    const updatedReply = replyInput.replace(/@(\w+),/g, '');
+
+    dispatch(addReply({ commentId: commentId, content: updatedReply, createdAt: 'Just now', replyingTo: username}));
+    setShowReplyInput(false);
+    setReplyInput('@'+username+',');
+  }
   return (
-    <div className="comment">
-      <Header/>
-      <p>Text</p>
-      <div className="interactions">
-      <Interaction/>
-      <button>?Reply</button>
+    <>
+    <article className="comment_grid">
+      
+      <div className="comment_header">
+        <div className="header header_section">
+          <img
+          className='user_image' 
+          src={user_image} 
+          alt={'headshot of ' + username} 
+          />
+          <h2>{username}</h2>
+          <b className="comment_date">{createdAt}</b>
+        </div>
       </div>
-    </div>
+
+      <p className="content">{content}</p>
+
+      <div className="score_controls votes">
+          <button 
+            type="button" 
+            className="upvote" 
+            onClick={handleUpvote}>
+              +
+          </button>
+
+          <button type="button" className="scores vote_count">{score}</button>
+          
+          <button 
+            type="button" 
+            className="downvote"
+            onClick={handleDownvote}>
+              -
+          </button>
+        </div>
+
+      <div className="vr_ interactions">
+        {/* score controls */}
+        <button
+        className="reply" 
+        type="button"
+        onClick={() => setShowReplyInput(true)}
+        ><img src="/assets/images/icon-reply.svg"/>Reply</button>
+      </div>
+    </article>
+
+    {showReplyInput && 
+      <div className={`${showReplyInput ?'drop' : ''} comments_text_area`}>
+        
+        <div className="comment_box reply_form_container">
+          <img
+          className="user_image" 
+          src={currentUser.image.webp} 
+          alt={'headshot of '+currentUser} />
+
+          <form onSubmit={handleReplySubmit}>
+            <textarea
+              type="text"
+              className="reply_input"
+              value={replyInput}
+              onChange={(e) => setReplyInput(e.target.value)}
+            ></textarea>
+            <button 
+            type="submit"
+            className="send reply_btn_"
+            >REPLY</button>
+          </form>
+        </div>
+
+      </div>
+      }
+      </>
   )
 }
