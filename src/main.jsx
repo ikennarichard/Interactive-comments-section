4d2526eb52eb3376712ce6c5d2cff1cf71155d4f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-13 20:28:48 -1100

    Add redux store to the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import store from './redux/store.js'
+import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 )
