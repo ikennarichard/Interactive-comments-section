1a518f4229001af0d12505bdc90f3845a8f6f3f4
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-13 20:22:51 -1100

    Add comment form

---
+++ src/components/AddCommentForm.jsx
@@ -0,0 +1,34 @@
+
+export default function AddCommentForm({
+  addComment,
+  handleSetContent,
+  content,
+}) {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    addComment()
+    handleSetContent('')
+  }
+  return (
+    <div>
+
+      <form 
+      onSubmit={handleSubmit} 
+      className="comment_box" >
+        <textarea 
+        type="text" 
+        placeholder='Add a comment...' 
+        value={content}
+        onChange={(e) => handleSetContent(e.target.value)}></textarea>
+        <div className="form_img_btn">
+          <img src="/assets/images/avatars/image-juliusomo.webp" alt="user_image" className="user_image"/>
+          <button
+          type='submit'
+          className="send add_comment"
+          >SEND</button>
+        </div>
+      </form>
+    </div>
+  )
+}
